af7aeb9f0b882e6e0d02ac8a65f19bf1065452ae
mikkotikkanen <mikko.tikkanen@gmail.com>
2025-05-11 14:18:25 +0300

    some common fixes

--- components/ButtonBase.tsx
+++ components/ButtonBase.tsx
@@ -64,7 +64,7 @@ export function ButtonBase({
   return (
     <Button
       className={clsx(
-        "rounded-full flex items-center gap-2 px-4 py-2 font-semibold",
+        "rounded-full flex items-center gap-2 px-4 py-2 font-semibold cursor-pointer",
         color === "primary" && "bg-black text-white border-black",
         color === "secondary" && "bg-white text-black border border-gray-200",
         variant === "outlined" && "border-2 border-dashed",
